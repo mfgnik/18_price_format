cdf149c5ca57b6360fe662e101f45a9aa8d6903d
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-25 12:18:52 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,7 +6,6 @@ class Test_format_price(unittest.TestCase):
     def test_integer_number_bigger_thousand(self):
         self.assertEqual(format_price('23112.000'), '23 112')
 
-
     def test_integer_number_less_thousand(self):
         self.assertEqual(format_price('23.000'), '23')
 
@@ -18,6 +17,18 @@ class Test_format_price(unittest.TestCase):
 
     def test_fractional_number_with_round(self):
         self.assertEqual(format_price('4.365'), '4.37')
+
+    def test_no_value(self):
+        self.assertIsNone(format_price(''))
+
+    def test_nothing_after_point(self):
+        self.assertEqual(format_price('5.'), '5')
+
+    def test_nothing_before_point(self):
+        self.assertEqual(format_price('0.543'), '0.54')
+
+    def test_no_point(self):
+        self.assertEqual(format_price('5'), 5)
 
 
 if __name__ == '__main__':
